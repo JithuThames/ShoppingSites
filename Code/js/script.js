e5a2d44571340dc508d9d87bb79ddf2697b170a6
JithuThames <10508308@mydbs.ie>
2019-08-06 13:59:14 +0100

    UpdatedGoogle signIn

--- Code/js/script.js
+++ Code/js/script.js
@@ -1,10 +1,12 @@
 function onSignIn(googleUser)
 {
+	alert("redirecting");
 	var profile=googleUser.getBasicProfile();
 	$(".g-signin2").css("display","none");
 	$(".data").css("display","block");
 	$("#pic").attr('src',profile.getImageUrl())
 	$("#email").text(profile.getEmail());
+	location.replace("OpeningPage.html");
 }
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
